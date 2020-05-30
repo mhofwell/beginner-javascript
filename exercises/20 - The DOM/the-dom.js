d99bbc72fefc8f4d0bb0bd5550dc9f7fe85f6a0c
Michael Hofweller <michael.hofweller@gmail.com>
2020-05-30 13:05:01 -0700

    DOM WORK

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,5 @@
+const abc = document.querySelector('p');
+const imgs = document.querySelectorAll('.item img');
+const item2 = document.querySelector('.item2');
+const item2image = item2.querySelector('img');
+console.log(item2);
