d2f59048e5b2d9d0295e3e2417f92a2e6d289c88
Michael Hofweller <michael.hofweller@gmail.com>
2020-06-18 18:04:55 -0700

    more updates to etch a sketch

--- exercises/33 - Etch-a-Sketch/etch-a-sketch.js
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch.js
@@ -1,6 +1,22 @@
-// select the elements on the page - canvas, shake button
+// select the elements on the page - canvas, context, shake button
+
+const canvas = document.querySelector('#etch-a-sketch');
+const ctx = canvas.getContext('2d');
+
+const shakeButton = document.querySelector('.shake');
 
 // set up our canvas for drawing
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 10;
+
+ctx.beginPath(); // start the drawing
+
+ctx.moveTo(200, 200); // starting point
+
+ctx.lineTo(200, 200);
+ctx.stroke(); // draw a line from where you started to where you drew your line too
 
 // write a draw function
 
