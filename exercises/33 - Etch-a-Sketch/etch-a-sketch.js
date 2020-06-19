2b894ef2f4e9e21003f555a340903347c1a63ffe
Michael Hofweller <michael.hofweller@gmail.com>
2020-06-18 18:55:04 -0700

    colors

--- exercises/33 - Etch-a-Sketch/etch-a-sketch.js
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch.js
@@ -4,16 +4,19 @@ const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 
 const shakeButton = document.querySelector('.shake');
-
+const MOVE_AMOUT = 10;
 // set up our canvas for drawing
 
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 10;
+ctx.lineWidth = MOVE_AMOUT;
+
+const hue = 0;
+// ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 
 const { width, height } = canvas;
-const x = Math.floor(Math.random() * width);
-const y = Math.floor(Math.random() * height);
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
 
 ctx.beginPath(); // start the drawing
 ctx.moveTo(x, y); // starting point for drawer
@@ -25,10 +28,46 @@ ctx.stroke(); // draw a line from where you started to where you drew your line 
 
 // write a draw function
 
-function draw() {}
+function draw({ key }) {
+        console.log(key);
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.strokeStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        // hue += 1;
+        // move our x and y values depending on what the user did
+        switch (key) {
+                case 'ArrowUp':
+                        y -= MOVE_AMOUT;
+                        break;
+                case 'ArrowDown':
+                        y += MOVE_AMOUT;
+                        break;
+                case 'ArrowLeft':
+                        x -= MOVE_AMOUT;
+                        break;
+                case 'ArrowRight':
+                        x += MOVE_AMOUT;
+                        break;
+                default:
+                        break;
+        }
+
+        ctx.lineTo(x, y);
+        ctx.stroke();
+}
 
 // write a handler for the keys
+
+function handleKey(e) {
+        if (e.key.includes('Arrow')) {
+                e.preventDefault();
+                draw({ key: e.key });
+                // console.log(e.key);
+                // console.log('handling key');
+        }
+}
 
 // clear or shake function
 
 // listen for arrow keys
+window.addEventListener('keydown', handleKey);
