e61efbf73c43116c0e7109b8e1efa5584c94a98b
Michael Hofweller <michael.hofweller@gmail.com>
2020-06-23 18:21:38 -0700

    done tabs

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,9 +1,33 @@
 const tabs = document.querySelector('.tabs');
 const tabButtons = tabs.querySelectorAll('[role="tab"]');
-const tabPanels = tabs.querySelectorAll('[role="tabPanel"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
 
-function handleTabClick(e) {
-        console.log(e);
+function handleTabClick(event) {
+        // hide all tab panels
+        tabPanels.forEach(panel => {
+                panel.hidden = true;
+        });
+        // mark all tabs as unselected
+
+        tabButtons.forEach(tab => {
+                // tab.ariaSelected = false;
+                tab.setAttribute('aria-selected', false);
+        });
+
+        // mark the clicked tab as selected
+        event.currentTarget.setAttribute('aria-selected', true);
+
+        // find the associated tab panel and show it
+
+        const { id } = event.currentTarget;
+        // const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+        // console.log(tabPanel);
+        // tabPanel.hidden = false;
+
+        // METHOD 2 - find in the array of tabPanels
+        console.log(tabPanels);
+        const tabPanel = tabPanels.find(panel => panel.getAttribute('aria-labelledby') === id);
+        console.log(tabPanel);
 }
 
 tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
