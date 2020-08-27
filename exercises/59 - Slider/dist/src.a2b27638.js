918911e3615c984476f252867bfeed3a996f186c
Michael Hofweller <michael.hofweller@gmail.com>
2020-08-27 15:51:34 -0700

    slider

--- exercises/59 - Slider/dist/src.a2b27638.js
+++ exercises/59 - Slider/dist/src.a2b27638.js
@@ -118,7 +118,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"src/index.js":[function(require,module,exports) {
-
+// / START !
 },{}],"../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
