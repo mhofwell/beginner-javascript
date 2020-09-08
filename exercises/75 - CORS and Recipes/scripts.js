ac2f8052afaf40767bee7679c72be20dfe3b86f9
Michael Hofweller <michael.hofweller@gmail.com>
2020-09-08 13:56:49 -0700

    CORS

--- exercises/75 - CORS and Recipes/scripts.js
+++ exercises/75 - CORS and Recipes/scripts.js
@@ -1,7 +1,9 @@
-const baseEndpoint = 'http://www.recipepuppy.com/api';
+const baseEndpoint = 'http://www.recipepuppy.com/api/';
+const proxy = 'https://cors-anywhere.herokuapp.com/';
 
 async function fetchRecipes(query) {
-        const res = await fetch(`${baseEndpoint}?q=${query}`);
+        const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
+        const data = await res.json();
 }
 
 fetchRecipes('pizza');
