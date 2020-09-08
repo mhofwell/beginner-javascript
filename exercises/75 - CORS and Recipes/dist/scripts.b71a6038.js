3688768e994432892992bd7779264238c65667f5
Michael Hofweller <michael.hofweller@gmail.com>
2020-09-08 14:55:41 -0700

    CORS

--- exercises/75 - CORS and Recipes/dist/scripts.b71a6038.js
+++ exercises/75 - CORS and Recipes/dist/scripts.b71a6038.js
@@ -120,13 +120,61 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 })({"scripts.js":[function(require,module,exports) {
 const baseEndpoint = 'http://www.recipepuppy.com/api/';
 const proxy = 'https://cors-anywhere.herokuapp.com/';
+const form = document.querySelector('form.search');
+const recipesGrid = document.querySelector('.recipes');
 
 async function fetchRecipes(query) {
   const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
   const data = await res.json();
+  return data;
+} // async function handleSubmit(e) {
+//         e.preventDefault();
+//         console.log(form.query.value);
+//         const el = e.currentTarget;
+//         // turn the form off
+//         el.submit.disabled = true;
+//         // submit the search
+//         console.log(el.query.value);
+//         const recipes = await fetchRecipes(el.query.value);
+//         console.log(recipes);
+//         el.submit.disabled = false;
+//         displayRecipes(recipes.results);
+// }
+
+
+async function handleSubmit(e) {
+  e.preventDefault();
+  const el = event.currentTarget;
+  fetchDisplay(form.query.value);
 }
 
-fetchRecipes('pizza');
+async function fetchDisplay(query) {
+  // turn the form off
+  form.submit.disabled = true; // submit the search
+
+  console.log(form.query.value);
+  const recipes = await fetchRecipes(query);
+  console.log(recipes);
+  form.submit.disabled = false;
+  displayRecipes(recipes.results);
+}
+
+function displayRecipes(recipes) {
+  console.log('creating html');
+  const html = recipes.map(recipe => `<div class = "recipe">
+        <h2>${recipe.title}</h2>
+        <p>${recipe.ingredients}</p>
+       
+        ${recipe.thumbnail && `<img src="${recipe.thumbnail}" alt="${recipe.title}"/>`}
+       
+        <a href="${recipe.href}">View Recipe -></a>
+       
+        </div>`);
+  recipesGrid.innerHTML = html.join('');
+}
+
+form.addEventListener('submit', handleSubmit);
+fetchDisplay('pizza');
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -155,7 +203,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54063" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57935" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- exercises/75 - CORS and Recipes/scripts.js
+++ exercises/75 - CORS and Recipes/scripts.js
@@ -1,9 +1,61 @@
 const baseEndpoint = 'http://www.recipepuppy.com/api/';
 const proxy = 'https://cors-anywhere.herokuapp.com/';
+const form = document.querySelector('form.search');
+const recipesGrid = document.querySelector('.recipes');
 
 async function fetchRecipes(query) {
         const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
         const data = await res.json();
+        return data;
 }
 
-fetchRecipes('pizza');
+// async function handleSubmit(e) {
+//         e.preventDefault();
+//         console.log(form.query.value);
+//         const el = e.currentTarget;
+//         // turn the form off
+//         el.submit.disabled = true;
+//         // submit the search
+//         console.log(el.query.value);
+//         const recipes = await fetchRecipes(el.query.value);
+//         console.log(recipes);
+//         el.submit.disabled = false;
+//         displayRecipes(recipes.results);
+// }
+
+async function handleSubmit(e) {
+        e.preventDefault();
+        const el = event.currentTarget;
+        fetchDisplay(form.query.value);
+}
+
+async function fetchDisplay(query) {
+        // turn the form off
+        form.submit.disabled = true;
+        // submit the search
+        console.log(form.query.value);
+        const recipes = await fetchRecipes(query);
+        console.log(recipes);
+        form.submit.disabled = false;
+        displayRecipes(recipes.results);
+}
+
+function displayRecipes(recipes) {
+        console.log('creating html');
+        const html = recipes.map(
+                recipe => `<div class = "recipe">
+        <h2>${recipe.title}</h2>
+        <p>${recipe.ingredients}</p>
+       
+        ${recipe.thumbnail && `<img src="${recipe.thumbnail}" alt="${recipe.title}"/>`}
+       
+        <a href="${recipe.href}">View Recipe -></a>
+       
+        </div>`
+        );
+        recipesGrid.innerHTML = html.join('');
+}
+
+form.addEventListener('submit', handleSubmit);
+
+fetchDisplay('pizza');
