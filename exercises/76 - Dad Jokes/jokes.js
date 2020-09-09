e07ba35a4ab69c160650be81e6b707f8cb396fbc
Michael Hofweller <michael.hofweller@gmail.com>
2020-09-09 10:15:12 -0700

    CORS

--- exercises/76 - Dad Jokes/jokes.js
+++ exercises/76 - Dad Jokes/jokes.js
@@ -1,10 +1,20 @@
+const jokeButton = document.querySelector('.getJoke');
+const jokeHolder = document.querySelector('.joke p');
+
 const buttonText = [
-  'Ugh.',
-  '🤦🏻‍♂️',
-  'omg dad.',
-  'you are the worst',
-  'seriously',
-  'stop it.',
-  'please stop',
-  'that was the worst one',
+        'Ugh.',
+        '🤦🏻‍♂️',
+        'omg dad.',
+        'you are the worst',
+        'seriously',
+        'stop it.',
+        'please stop',
+        'that was the worst one',
 ];
+
+async function fetchJoke() {
+        const response = await fetch('https://icanhazdadjoke.com/');
+        const joke = console.log(response);
+}
+
+fetchJoke();
