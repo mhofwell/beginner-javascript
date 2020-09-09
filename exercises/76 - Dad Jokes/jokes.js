b47d388103b374b0768ffbd525a96786346f2fdd
Michael Hofweller <michael.hofweller@gmail.com>
2020-09-09 12:20:16 -0700

    CORS

--- exercises/76 - Dad Jokes/jokes.js
+++ exercises/76 - Dad Jokes/jokes.js
@@ -1,5 +1,6 @@
 const jokeButton = document.querySelector('.getJoke');
 const jokeHolder = document.querySelector('.joke p');
+const loader = document.querySelector('.loader');
 
 const buttonText = [
         'Ugh.',
@@ -13,8 +14,39 @@ const buttonText = [
 ];
 
 async function fetchJoke() {
-        const response = await fetch('https://icanhazdadjoke.com/');
-        const joke = console.log(response);
+        // turn loader on
+        loader.classList.remove('hidden');
+        jokeButton.classList.add('hidden');
+        const response = await fetch('https://icanhazdadjoke.com/', {
+                headers: {
+                        Accept: 'application/json',
+                },
+        });
+        const data = await response.json();
+        loader.classList.add('hidden');
+        jokeButton.classList.remove('hidden');
+        return data;
 }
 
-fetchJoke();
+function randomItemFromArray(arr, not) {
+        const item = arr[Math.floor(Math.random() * arr.length)];
+        return item === not ? randomItemFromArray(arr, not) : item;
+}
+
+// function randomItemFromArray(arr, not) {
+//         const item = arr[Math.floor(Math.random() * arr.length)];
+//         console.log(item);
+//         // if (item === not) {
+//         //         console.log('omg');
+//         //         return randomItemFromArray(arr, not);
+//         // }
+//         // return item;
+// }
+
+async function handleClick() {
+        const { joke } = await fetchJoke();
+        jokeHolder.textContent = joke;
+        jokeButton.textContent = randomItemFromArray(buttonText, jokeButton.textContent);
+}
+
+jokeButton.addEventListener('click', handleClick);
